95e221a3e8041401408d2df9f4c88b68bbda1088
seven-mile <i@7li.moe>
2024-03-25 21:01:41 +0800

    fix: use 12.2 nsys ref: https://forums.developer.nvidia.com/t/profiling-nccl-2-14-3-leads-to-segmentation-fault/242371

--- scripts/nsys_wrap.sh
+++ scripts/nsys_wrap.sh
@@ -7,16 +7,19 @@ if [[ -z "${NSYS_NODE_INTERVAL}" ]]; then
     NSYS_NODE_INTERVAL=1
 fi
 
+NSYS_BIN=/opt/app/cuda/12.2/bin/nsys
+
+# TODO: capture all ranks
 if [ "${SLURM_LOCALID}" -eq 0 ] && [ "$(( SLURM_NODEID % NSYS_NODE_INTERVAL ))" -eq 0 ];
 then
-    nsys profile \
+    $NSYS_BIN profile \
         -f true \
         -o ${NSYS_OUTPUT}_node${SLURM_NODEID} \
         -c cudaProfilerApi \
         --trace cuda,nvtx,cudnn,osrt \
         --export sqlite \
-        $@
+        $@ 2>&1 | tee output/logs/rank${SLURM_PROCID}.log
 else
-    $@
+    $@ 2>&1 | tee output/logs/rank${SLURM_PROCID}.log
 fi
 sleep 30  # wait for nsys to complete
